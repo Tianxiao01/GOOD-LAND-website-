ed57bad3c00b5957c09743fb178be6bcbf8082bb
Tianxiao Lou <TianxiaoLou@outlook.com>
2024-12-30 15:21:26 -0500

    fadeaway animation added

--- src/pages/Home/Intro_text.tsx
+++ src/pages/Home/Intro_text.tsx
@@ -1,16 +1,44 @@
 import Bottom from "./Bottom";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 interface Props {
   layout: string;
   content: string[];
   path: string;
+  height_fadeaway: number;
 }
 
-const IntroText = ({ layout, content, path }: Props) => {
+const IntroText = ({ layout, content, path, height_fadeaway }: Props) => {
+  const [opacity, setOpacity] = useState(1);
+
+  const handleScroll = () => {
+    const viewportHeight = window.innerHeight;
+    const scrollY = (window.scrollY / viewportHeight) * 100;
+    const Height_begin_fade = height_fadeaway;
+
+    const newopacity =
+      scrollY >= Height_begin_fade
+        ? Math.max(1 - (scrollY - Height_begin_fade) / 45, 0)
+        : 1;
+    setOpacity(newopacity);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <Link to={path} style={{ textDecoration: "none", color: "inherit" }}>
-      <div className={layout} id="IntroText" style={{ cursor: "pointer" }}>
+      <div
+        className={layout}
+        id="IntroText"
+        style={{ cursor: "pointer", opacity: opacity }}
+      >
         <div>
           {content.map((item, index) => (
             <div key={index}>{item}</div>
