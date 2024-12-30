3a6c88497d4a48838befb3f4e181d726899b503a
punchdii <sunqiche@outlook.com>
2024-12-30 00:23:51 -0500

    Added routing for subpage LongTerm, ShortTerm, and Future

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
+//importing subpages
 import Home from "./pages/Home/Home";
-
+import LongTerm from "./pages/LongTerm/LongTerm";
+import Future from "./pages/Future/Future";
+import ShortTerm from "./pages/ShortTerm/ShortTerm";
+//css
 import "./App.css";
+//React Router
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/LongTerm" element={<LongTerm />} />
+          <Route path="/Future" element={<Future />} />
+          <Route path="/ShortTerm" element={<ShortTerm />} />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
