1a088cb79cd1e405c4d658ddfb5ceeea4459f812
Tianxiao01 <tianxiaolou@outlook.com>
2025-01-04 17:23:20 -0500

    bottom info added, better navbar

--- src/pages/Home/Bottom.tsx
+++ src/pages/Home/Bottom.tsx
@@ -1,5 +1,10 @@
 const Bottom = () => {
-  return <div className="Bottom"></div>;
+  return <div className="Bottom">
+        <div className="Additional_info">Contact: <br/>
+        phone: (+1) 1234567890<br/>
+        email: 1234567890@example.com
+        </div>
+       </div>;
 };
 
 export default Bottom;
