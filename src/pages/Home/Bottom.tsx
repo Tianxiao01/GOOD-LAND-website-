ea899ce49dcde6285610fdbf59cb17d0477a3a27
Tianxiao01 <tianxiaolou@outlook.com>
2025-01-05 19:06:26 -0500

    Bottom info improv

--- src/pages/Home/Bottom.tsx
+++ src/pages/Home/Bottom.tsx
@@ -1,10 +1,16 @@
 const Bottom = () => {
   return <div className="Bottom">
-        <div className="Additional_info">Contact: <br/>
+        <div className="Additional_info1">Contact: <br/>
         phone: (+1) 1234567890<br/>
         email: 1234567890@example.com
         </div>
+        <div className="Additional_info2">Address: <br/>
+        Main: 123 AVE example, province, A1A B2B<br/>
+        Branch: 123 AVE example, province, A1A B2B
+        </div>
        </div>;
 };
+
+
 
 export default Bottom;
