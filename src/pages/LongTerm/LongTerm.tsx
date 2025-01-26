349ad063edbc2086fdbf83535feb32c82e17775c
punchdii <sunqiche@outlook.com>
2025-01-25 23:30:32 -0500

    update

--- src/pages/LongTerm/LongTerm.tsx
+++ src/pages/LongTerm/LongTerm.tsx
@@ -4,10 +4,10 @@ import Card from "./Card";
 const LongTerm = () => {
     return (
         <div>
-            <h1>Long Term </h1>
-            <h1>Long Term </h1>
-            <h1>Long Term </h1>
-            <h1>Long Term </h1>
+            <h1> </h1>
+            <h1> </h1>
+            <h1> </h1>
+            <h1> </h1>
 
             <Card />
 
