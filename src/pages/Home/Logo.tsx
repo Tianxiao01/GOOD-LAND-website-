bebf642ca9073c84bb136a4b25892b5541498e97
Tianxiao01 <tianxiaolou@outlook.com>
2025-02-08 17:15:55 -0500

    home page mobile compability progresing _stage1

--- src/pages/Home/Logo.tsx
+++ src/pages/Home/Logo.tsx
@@ -1,12 +1,19 @@
 import GOOD_LAND_logo from "../../assets/good_land_logo.png";
 
-const Logo = () => {
+interface Props{
+  size:string
+  top:string
+}
+
+
+
+const Logo = ({size,top}:Props) => {
   return (
     <img
       alt="GOOD LAND logo"
       src={GOOD_LAND_logo}
       className="img"
-      style={{ cursor: "pointer" }}
+      style={{ cursor: "pointer",width: `${size}`,top: `${top}` }}
     ></img>
   );
 };
